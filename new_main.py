1e274f4d99cfbdc498e4d59833dba12051852b21
olegsmit <oleg.smit@gmail.com>
2023-03-07 21:48:57 +0200

    in process

--- new_main.py
+++ new_main.py
@@ -1,36 +1,87 @@
 import sys
 from pathlib import Path
+import re
 from normalize import normalize
+import shutil
 
 
-folder_to_del = []
-def move_file(target_path:Path, file:Path):
-    file.replace(target_path / file.name)
+CATEGORIES = {}
+folders_to_del = []
+
+def load_categories() -> dict:
+
+    with open("categories.txt") as file:
+        for line in file:
+            value: object
+            key, *value = re.split(': |, ', line[:-1])
+            CATEGORIES[key] = value
 
 
-def rm_dir(folder_for_scan:Path):
-    for folder in folder_for_scan.glob('**'):
-        folder_to_del.append(folder)
-    del_folder = folder_to_del[::-1]
-    for folder in del_folder[0:-1]:
-        try:
-            folder.rmdir()
-        except OSError as err:
-            print(err)
-def scan(folder:Path):
+def move_file(file: Path):
+    for key, value in CATEGORIES.items():
+        if file.suffix in value:
+            cr_dir = Path(folder_for_scan / key)
+            cr_dir.mkdir(exist_ok=True, parents=True)
+            file.replace(folder_for_scan / key / file.name)
+
+
+def rm_dir(folder_for_rm: Path):
+    # for folder in folder_for_rm.glob('**'):
+    #     folders_to_del.append(folder)
+    # del_folders = folders_to_del[::-1]
+    # for folder in del_folders[0:-1]:
+    try:
+        folder_for_rm.rmdir()
+    except OSError:
+        print(f"Folder '{folder_for_rm}' is not empty.")
+
+
+def unpack(file):
+    for key, value in CATEGORIES.items():
+        if key == 'archives':
+            if file.suffix in CATEGORIES[key]:
+                try:
+                    foldder_for_file = Path(folder_for_scan / key / file.stem)
+                    foldder_for_file.mkdir(exist_ok=True, parents=True)
+                    shutil.unpack_archive(file, foldder_for_file)
+                    file.unlink()
+
+                except shutil.ReadError:
+                    print(f'{file} - не вдалося розпакувати!')
+                    foldder_for_file.rmdir()
+                    return None
+                break
+
+
+def scan(folder: Path):
+
+    load_categories()
+
     for item in folder.iterdir():
         if item.is_dir():
-            if item.name not in ('archives', 'video', 'audio', 'documents', 'images', 'OTHER'):
-                FOLDERS.append(item)
+            if item.name not in ('archives', 'video', 'audio', 'documents', 'images'):
                 scan(item)
+                rm_dir(item)
             continue
+        else:
+            normalize(item.name)
+            unpack(item)
+            move_file(item)
 
-    for item in folder_for_scan.glob('**/*.*'):
-        move_file(folder_for_scan, item)
 
-    rm_dir(folder_for_scan)
 
-    normalize()
+
+    # for item in folder.iterdir():
+    #     if item.is_dir():
+    #         if item.name not in ('archives', 'video', 'audio', 'documents', 'images'):
+    #             rm_dir(item)
+    #         continue
+
+
+
+
+    # rm_dir(folder)
+
 
 
 # print(type(path))
